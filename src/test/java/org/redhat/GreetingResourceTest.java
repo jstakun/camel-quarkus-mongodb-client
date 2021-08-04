356f70bb7735c7ae9856efbc027a433b03d5b874
jstakun <jstakun@redhat.com>
2021-08-04 18:33:23 +0200

    fixed unit test

--- src/test/java/org/redhat/GreetingResourceTest.java
+++ src/test/java/org/redhat/GreetingResourceTest.java
@@ -12,10 +12,10 @@ public class GreetingResourceTest {
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/hello-resteasy")
+          .when().get("/camel/v1/cache/test/10")
           .then()
              .statusCode(200)
-             .body(is("Hello RESTEasy"));
+             .body(is("[]"));
     }
 
 }
