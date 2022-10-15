1a76c210e9cb0b9d3e2162e6cb78355a67d2b582
jstakun <jstakun@redhat.com>
2022-10-15 15:44:47 +0200

    leader election

---
+++ src/main/java/org/redhat/ClusterLockProducer.java
@@ -0,0 +1,41 @@
+package org.redhat;
+
+import io.quarkus.arc.profile.IfBuildProfile;
+import io.quarkus.arc.profile.UnlessBuildProfile;
+import org.apache.camel.CamelContext;
+import org.apache.camel.cluster.CamelClusterService;
+import org.apache.camel.component.file.cluster.FileLockClusterService;
+import org.apache.camel.component.kubernetes.cluster.KubernetesClusterService;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+
+@ApplicationScoped
+public class ClusterLockProducer {
+
+    @ConfigProperty(name = "namespace")
+    Optional<String> namespace;
+
+    @Produces
+    @UnlessBuildProfile("prod")
+    public CamelClusterService fileLockClusterService(CamelContext camelContext) throws Exception {
+        FileLockClusterService service = new FileLockClusterService();
+        service.setRoot("cluster");
+        service.setAcquireLockDelay(1, TimeUnit.SECONDS);
+        service.setAcquireLockInterval(1, TimeUnit.SECONDS);
+        return service;
+    }
+
+    @Produces
+    @IfBuildProfile("prod")
+    public CamelClusterService kubernetesClusterService(CamelContext camelContext) {
+        KubernetesClusterService service = new KubernetesClusterService();
+        if (namespace.isPresent()){
+            service.setKubernetesNamespace(namespace.get());
+        }
+        return service;
+    }
+}
