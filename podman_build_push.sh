f28b483cbfce5ddc81eeddf054a454a51d3d9559
jstakun <jstakun@redhat.com>
2022-11-08 13:13:22 +0100

    fix link

--- podman_build_push.sh
+++ podman_build_push.sh
@@ -1,5 +1,5 @@
 VERSION=latest
-AUTHFILE=/home/jstakun/camelquarkusmongoclient.auth
+AUTHFILE=~/camelquarkusmongoclient.auth
 #podman login quay.io
 podman build -f Containerfile.distroless . -t quay.io/jstakun/camel-quarkus-mongodb-client:$VERSION
 podman push --authfile $AUTHFILE quay.io/jstakun/camel-quarkus-mongodb-client:$VERSION
